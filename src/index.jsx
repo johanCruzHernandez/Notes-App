0dda3180894e8a849917f65986a20f0435b0b92d
Johan Cruz Hernandez <129089277+johanCruzHernandez@users.noreply.github.com>
2023-10-10 20:49:29 -0400

    Create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import createRoot from 'react-dom/client';
+import "./styles.scss";
+import App from "./App";
+
+// const root = createRoot(document.getElementById('main'));
+// root.render(<App />);
+
+ReactDOM.render(<App />, document.getElementById("main"));
